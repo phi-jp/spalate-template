0bffe0bfbcd6aff4cbbb3b19ec9b4e0ec6c7327c
phi <phi1618jp@gmail.com>
2019-05-04 18:06:13 +0900

    setup firestore

---
+++ public/scripts/flarestore.js
@@ -0,0 +1,19 @@
+;((global) => {
+
+  var flarestore = {
+    init: () => {
+      flarestore.db = firebase.firestore();
+    },
+
+    get: () => {
+
+    },
+
+    watch: () => {
+
+    },
+  };
+
+  global.flarestore = flarestore;
+
+})(this);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async () => {
+  firebase.initializeApp(config.firebase);
+  flarestore.init();
+
+  var user = await auth.init();
+
+  if (!user) {
+    auth.signInAnonymously();
+  }
+
   spalate.start();
 });
