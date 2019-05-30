0db44e278eb924a4c218540d0cd88c1d64f610ba
phi <phi1618jp@gmail.com>
2019-05-30 19:25:33 +0900

    setup admin

--- template/admin/app/assets/scripts/router.js
+++ template/admin/app/assets/scripts/router.js
@@ -3,21 +3,16 @@
 ;(function(exports) {
 
   exports.map = {
-    '/groups/:id': {
-      tag: 'page-groups-single',
+    '/auth': {
+      tag: 'page-auth',
     },
-    '/users/:id': {
-      tag: 'page-users-single',
-    },
-    '/mypage': {
-      tag: 'page-users-single',
+    '/:page/:id': {
+      tag: 'page-detail',
     },
     '/:page': {
-      tag: function(req, res) {
-        return 'page-' + req.params.page;
-      },
+      tag: 'page-list',
     },
-    '/': {
+    '/*': {
       tag: 'page-index',
     }
   };
