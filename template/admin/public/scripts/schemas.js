269f49af4da4a9d9a600af0ca59c6465ab849c13
phi <phi1618jp@gmail.com>
2019-05-31 16:19:26 +0900

    fix admin

--- template/admin/public/scripts/schemas.js
+++ template/admin/public/scripts/schemas.js
@@ -25,6 +25,24 @@
         { key: 'memo', label: '備考', type: 'multitext' },
       ],
     },
+    groups: {
+      label: 'グループ',
+      collection: 'groups',
+
+      show: [
+        { label: 'index', type: 'index', class: 'col1' },
+        { label: 'ID',    type: 'id', class: 'col1' },
+        { label: 'title',  type: 'label', key: 'title', class: 'col2' },
+        { label: 'profile',  type: 'text', key: 'profile' },
+      ],
+      edit: [
+        { key: 'name', label: '成分名', type: 'text', input_type: 'text' },
+        { key: 'description', label: '詳細', type: 'multitext' },
+        { key: 'amount', label: '成分量', type: 'text', input_type: 'text' },
+        { key: 'material', label: '原料名', type: 'text', input_type: 'text' },
+        { key: 'memo', label: '備考', type: 'multitext' },
+      ],
+    },
   };
 
   global.dataTransfer = {
