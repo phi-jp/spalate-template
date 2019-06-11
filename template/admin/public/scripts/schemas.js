09f8d2ea8801d1363403dff11b75698c79378f3c
phi <phi1618jp@gmail.com>
2019-06-12 08:30:37 +0900

    fix style

--- template/admin/public/scripts/schemas.js
+++ template/admin/public/scripts/schemas.js
@@ -25,9 +25,27 @@
         { key: 'memo', label: '備考', type: 'multitext' },
       ],
     },
-    groups: {
-      label: 'グループ',
-      collection: 'groups',
+    cards: {
+      label: 'カード',
+      collection: 'cards',
+
+      show: [
+        { label: 'index', type: 'index', class: 'col1' },
+        { label: 'ID',    type: 'id', class: 'col1' },
+        { label: 'title',  type: 'label', key: 'title', class: 'col2' },
+        { label: 'profile',  type: 'text', key: 'profile' },
+      ],
+      edit: [
+        { key: 'name', label: '成分名', type: 'text', input_type: 'text' },
+        { key: 'description', label: '詳細', type: 'multitext' },
+        { key: 'amount', label: '成分量', type: 'text', input_type: 'text' },
+        { key: 'material', label: '原料名', type: 'text', input_type: 'text' },
+        { key: 'memo', label: '備考', type: 'multitext' },
+      ],
+    },
+    attributes: {
+      label: '属性',
+      collection: 'attributes',
 
       show: [
         { label: 'index', type: 'index', class: 'col1' },
@@ -56,5 +74,30 @@
       return item;
     },
   };
+
+  global.menus = [
+    {
+      label: 'カードマスタ',
+      items: [
+        {
+          label: 'カード一覧',
+          link: '/cards',
+        },
+        {
+          label: '属性一覧',
+          link: '/attributes',
+        },
+      ]
+    },
+    {
+      label: 'その他',
+      items: [
+        {
+          label: '管理者',
+          link: '/users',
+        },
+      ]
+    },
+  ];
 
 })(this);
