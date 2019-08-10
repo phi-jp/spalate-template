87b37acdbd48c535704a0e0712056f4e0c78ce03
phi <phi1618jp@gmail.com>
2019-08-10 20:04:08 +0900

    fix scheme

--- template/admin/public/scripts/admin_fujimi.js
+++ template/admin/public/scripts/admin_fujimi.js
@@ -69,7 +69,19 @@
         { label: '詳細',  type: 'text', key: 'data.description', class: '' },
       ],
       edit: [
-        { label: '名前',  type: 'text', key: 'data.name', class: 'col12' },
+        { label: 'サプリ名',  type: 'text', key: 'data.name', class: 'col12' },
+        { label: 'サプリ名(日本語)',  type: 'text', key: 'data.name_ja', class: 'col12' },
+        { label: '詳細',  type: 'multitext', key: 'data.description', class: 'col12' },
+        { label: '形状',  type: 'text', key: 'data.shape', class: 'col4' }, // todo
+        { label: '成分量',  type: 'text', key: 'data.capacity', class: 'col4' },
+        { label: '栄養成分',  type: 'multitext', key: 'data.component', class: 'col12' },
+        { label: '成分一覧',  type: 'text', key: 'data.ingredients', class: 'col12' }, // todo
+        { label: 'タイトル1',  type: 'text', key: 'data.01_title', class: 'col12' },
+        { label: '説明1',  type: 'multitext', key: 'data.01_description', class: 'col12' },
+        { label: 'タイトル2',  type: 'text', key: 'data.02_title', class: 'col12' },
+        { label: '説明2',  type: 'multitext', key: 'data.02_description', class: 'col12' },
+        { label: 'タイトル3',  type: 'text', key: 'data.02_title', class: 'col12' },
+        { label: '説明3',  type: 'multitext', key: 'data.03_description', class: 'col12' },
       ],
     },
     // 診断
