05c29c9b71cf2323e275105ccef68775500d2b65
phi <phi1618jp@gmail.com>
2019-12-10 02:13:14 +0900

    エラー処理周りを整理

--- template/default/public/scripts/flarebase.js
+++ template/default/public/scripts/flarebase.js
@@ -3,10 +3,52 @@
 
   var flarebase = {
     auth: {
-      signInWithProvider: async () => {
-        var provider = new firebase.auth.TwitterAuthProvider();
-        var res = await firebase.auth().signInWithPopup(provider);
+      async createUserWithEmailAndPassword(email, password) {
+        try {
+          var res = await firebase.auth().createUserWithEmailAndPassword(email, password);;
+          var data = this._normalizeResponse(res);
+          return data;
+        }
+        catch(e) {
+          e.error_message = {
+            'auth/email-already-in-use': 'このメールアドレスはすでに使用されています',
+            'auth/invalid-email': 'メールアドレスの形式が間違っています',
+            'uth/operation-not-allowed': 'メールアドレス/パスワードによるアカウント登録が有効化されていません',
+            'auth/weak-password': 'パスワードのセキュリティが弱すぎます',
+          }[e.code];
 
+          throw e;
+        }
+      },
+      async signInWithProvider(providerId) {
+        try {
+          var provider = this.idToProvider(providerId);
+          var res = await firebase.auth().signInWithPopup(provider);
+          var data = this._normalizeResponse(res);
+
+          return data;
+        }
+        catch (e) {
+          e.error_message = {
+            'auth/popup-closed-by-user': 'ログイン処理が中断されました',
+          }[e.code];
+
+          throw e;
+        }
+      },
+
+      idToProvider(providerId) {
+        var provider = {
+          'google.com': firebase.auth.GoogleAuthProvider,
+          'twitter.com': firebase.auth.TwitterAuthProvider,
+          'facebook.com': firebase.auth.FacebookAuthProvider,
+        }[providerId];
+
+        return new provider;
+      },
+
+      _normalizeResponse(res) {
+        // 共通情報
         var user = {
           display_name: res.user.displayName,
           screen_name: res.user.uid,
@@ -14,12 +56,17 @@
           email: res.user.email,
           verified_email: res.user.emailVerified,
           image: res.user.photoURL,
+          // 匿名かどうか
+          isAnonymous: res.user.isAnonymous,
+          // 新しいユーザーかどうか
+          isNewUser: res.additionalUserInfo.isNewUser,
         };
 
         var userInfo = res.additionalUserInfo;
 
+        // sns 差分を吸収
         if (userInfo.providerId === 'google.com') {
-
+          // TODO: 
         }
         else if (userInfo.providerId === 'twitter.com') {
           user.screen_name = userInfo.profile.screen_name;
@@ -27,8 +74,16 @@
           user.image = userInfo.profile.profile_image_url_https.replace('_normal', '');
         }
         else if (userInfo.providerId === 'facebook.com') {
-          
+          // TODO: facebook api で大きな画像を取ってきて差し替える
         }
+        else if (userInfo.providerId === 'password') {
+          // TODO: 
+        }
+
+        return {
+          user: user,
+          providerId: userInfo.providerId,
+        };
       },
     },
   };
